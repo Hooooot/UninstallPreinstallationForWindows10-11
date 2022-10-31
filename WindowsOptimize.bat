d51ef2545e853315e10f7020982253100ba382f1
Hooooot <30996380+Hooooot@users.noreply.github.com>
2022-10-31 20:25:22 +0800

    Update WindowsOptimize.bat

--- WindowsOptimize.bat
+++ WindowsOptimize.bat
@@ -32,14 +32,13 @@ goto:eof
 
 :win11_optimize
     @REM win11关闭小组件
-    reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced                  /v TaskbarDa             /t REG_DWORD /d 0 /f
+    reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced                    /v TaskbarDa             /t REG_DWORD /d 0 /f
     @REM win11关闭任务栏多任务按钮
-    reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced                  /v ShowTaskViewButton    /t REG_DWORD /d 0 /f
+    reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced                    /v ShowTaskViewButton    /t REG_DWORD /d 0 /f
     @REM win11关闭windows跟踪应用启动，如果使用windows搜索则不推荐关闭
     @REM reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced                  /v Start_TrackProgs      /t REG_DWORD /d 0 /f
     @REM win11关闭二级菜单
     reg add "HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /v /f /ve
-    taskkill /f /im explorer.exe & start explorer.exe
 goto:eof
 
 
@@ -47,6 +46,8 @@ goto:eof
 :common_optimize
     @REM 桌面显示此电脑图标
     reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {20D04FE0-3AEA-1069-A2D8-08002B30309D}      /t REG_DWORD /d 0 /f
+    @REM 显示已知扩展名
+    reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced                       /v HideFileExt                                 /t REG_DWORD /d 0 /f
 goto:eof
 
 
@@ -57,7 +58,7 @@ powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *communi*
 powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *Clipchamp* | Remove-AppxPackage }"
 powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *ZuneMusic* | Remove-AppxPackage }"
 powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *bing* | Remove-AppxPackage }"
-powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *people* | Remove-AppxPackage }"
+@REM powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *people* | Remove-AppxPackage }"
 powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *phone* | Remove-AppxPackage }"
 powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *solit* | Remove-AppxPackage }"
 powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *Sticky* | Remove-AppxPackage }"
@@ -70,25 +71,42 @@ powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *Getstart
 powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *OfficeHub* | Remove-AppxPackage }"
 powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *GamingApp* | Remove-AppxPackage }"
 powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *549981C3F5F10* | Remove-AppxPackage }"
+powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *SkypeApp* | Remove-AppxPackage }"
+powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *MixedReality* | Remove-AppxPackage }"
+powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppxPackage *MSPaint* | Remove-AppxPackage }"
 
-powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppXProvisionedPackage -online | Remove-AppxProvisionedPackage -online }"
+@REM powershell -ExecutionPolicy Unrestricted -Command "& { Get-AppXProvisionedPackage -online | Remove-AppxProvisionedPackage -online }"
+set onedrivePath=
+for /f "tokens=*" %%i in ('dir %localappdata%\Microsoft\OneDrive\ /AD /B') do (
+    if exist "%localappdata%\Microsoft\OneDrive\%%i\OneDriveSetup.exe" (
+        set onedrivePath="%localappdata%\Microsoft\OneDrive\%%i\OneDriveSetup.exe"
+    )
+)
+if "%onedrivePath%" NEQ "" (
+    %onedrivePath% /uninstall
+    timeout /T 3 /NOBREAK
+    taskkill /f /im OneDrive.exe
+    taskkill /f /im explorer.exe
+    del /Q "%localappdata%\Microsoft\OneDrive\"
+    start explorer.exe
+)       
 
-"%localappdata%\Microsoft\OneDrive\Update\OneDriveSetup.exe" /uninstall
-del /Q /"%localappdata%\Microsoft\OneDrive\Setup"
-del /Q /"%localappdata%\Microsoft\OneDrive\logs"
 goto:eof
 
 
 :main
-if %OSVER%=="win10" (
+taskkill /f /im explorer.exe
+if "%OSVER%"=="win10" (
+    echo win 10
     call :win10_optimize
 )
-if %OSVER%=="win11" (
+if "%OSVER%"=="win11" (
+    echo win 11
     call :win11_optimize
 )
-
 call :common_optimize
-
+start explorer.exe
 call :remove_app
-
+start explorer.exe
+pause 
 goto:eof
